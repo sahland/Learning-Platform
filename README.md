e7c42e37deedf73e7a5c7cd2da42adb198076e99
Evgeniy Akimushkin <j.akimushkin2003@gmail.com>
2024-03-25 21:51:03 +0300

    chore(): added project to repository

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Learning-Platform
+
+- [9.5-Team repository](https://github.com/sahland/9.5-Team)
+- [Learning-Platform.Frontend](https://github.com/sahland/Learning-Platform.Frontend/tree/4836bdb011d57669a344f742bb8a1b6a46261995)
---
+++ src/test/java/com/knitwit/knitwit/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knitwit.knitwit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
